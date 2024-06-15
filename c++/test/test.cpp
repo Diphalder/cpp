9c1bc156690d797188e95ada4ce9ed13ff800087
Dip Halder <47663440+Diphalder@users.noreply.github.com>
2024-06-16 05:32:28 +0600

    fen

--- c++/test/test.cpp
+++ c++/test/test.cpp
@@ -2,6 +2,7 @@
 #define lld             long long int
 #define llf             long double
 #define pb              push_back
+#define popb            pop_back
 #define mp              make_pair
 #define in              insert
 #define loopN(n)        for(lld i=0;i<n;i++)
@@ -18,7 +19,7 @@
 #define INF             2e18
 #define FILE            freopen("input.txt","r",stdin);freopen("output.txt","w",stdout)
 #define pi              (2*acos(0.0))
-#define mod             (1e9+7)
+#define mod             (lld)(1e9+7)
 #define ISO             ios::sync_with_stdio(false);cin.tie(0)
 #define mem(a,b)        memset(a,b,sizeof(a))
 #define case(z)         cout<<"Case "<<z<<": "
@@ -38,6 +39,10 @@
 #define ison(m,p)       ((bool)(m&(1LL<<p)))
 #define flip(m,p)       (m^(1LL<<p))
 #define nbitON(p)       ((1LL<<p)-1)
+//__________Fenwick Tree__________________________
+#define rmvLastBit(n)   n=n-(n&-n)
+#define child_node(n)   n=n-(n&-n)
+#define par_node(n)     n=n+(n&-n)
 //_______________________________________________
 using namespace std;
 
@@ -52,19 +57,98 @@ map< , > :: iterator it
 lld fx[]= {1,0,-1, 0,1, 1,-1,-1};
 lld fy[]= {0,1,0,-1,1,-1,1,-1};
 //_______________________________________________
-lld const mx=1e5+10;
 
-
-lld a[mx];
+lld const sz = 1e5+5;
+lld tree[sz];
 lld n;
+
+vector<pair<lld,lld>>a;
+
+void ini()
+{
+    //tree range [1,n]
+    loop(1,n)
+    {
+        tree[i]=0;
+    }
+}
+
+void set_val(lld p, lld val)
+{
+    while(p<=n)
+    {
+        tree[p]+=val;
+        tree[p]%=mod;
+        p=par_node(p);
+    }
+
+}
+
+lld sum_upto(lld p)
+{
+    lld sum=0;
+    while(p)
+    {
+        sum+=tree[p];
+        sum%=mod;
+        p=child_node(p);
+    }
+    return sum%mod;
+}
+
+bool cmp(pair <int,int> a, pair <int,int> b)
+{
+    if(a.first != b.first)
+        return a.first<b.first;
+    return a.second>b.second;
+}
 
 
 void slv()
 {
+    
     cin>>n;
-    loopN(n)cin>>a[i];
+    lld x;
+    pair<lld,lld>pr;
+    map<lld,lld> minpos;
+    
+    loopN(n)
+    {
+        cin>>x;
+        pr=mp(x,i);
+        a.pb(pr);
 
-    cout<<fun()<<endl;
+        if(minpos[x]==0)
+        {
+            minpos[x]=i+1;
+        }
+        else
+        {
+            minpos[x]=min(minpos[x],i+1);
+        }
+        
+    }
+
+    ini();
+    sort(all(a),cmp);
+
+    lld mpos,val,pos;
+
+    loopN(n)
+    {
+        
+   
+        pos=a[i].ss+1;
+        val=sum_upto(pos-1);
+        set_val(pos,val+1);
+
+ 
+
+
+    }
+    a.clear();
+
+    cout<<sum_upto(n)%mod<<endl;
 
 
 }
